fc091ab15774745d9e3aac57be77b808c2dfebba
rdowd <rjarrettdowd@yahoo.com>
2020-04-23 23:03:16 -0400

    set up inital migration file.

---
+++ data/migrations/20200423224727_initial.js
@@ -0,0 +1,17 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable("cars", (table) => {
+        table.increments("id")
+        table.integer("vin").notNull().unique()
+        table.text("make").notNull()
+        table.text("model").notNull()
+        table.integer("mileage").notNull()
+        table.text("transmission")
+        table.text("status")
+
+    })
+}
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists("cars")
+}
